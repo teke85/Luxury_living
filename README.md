17b7de87a6e32b0d8a90234c5a2f58ccdcc211b0
teke <mutau85@gmail.com>
2022-09-15 22:11:23 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
 # Luxury_Living Capstone Project
-In this Capstone project I will build a website for a highend business offering luxury accomodation company in Zambia. 
+
+In this Capstone project I will build a website for a highend business offering luxury accomodation and various other services for in Zambia. The business has been in operation for over ten years and most of its clientele consists of international businesses in the uk and USA.
+
+## Built With
+
+- HTML
+- CSS
+- Javascript
+
+## Live Demo
+
+- [Demo link](https://teke85.github.io/Luxury_living/)
+- [Video link](https://www.loom.com/share/e482f101625a44768fe2e80cbecdf2cc)
+
+## Getting Started
+
+**To get a local copy up and running follow these simple example steps.**
+
+### Prerequisites
+
+A basic understanding of Git, Github HTML and CSS is needed to setup and modify this project.
+
+You also need an editor like VS Code or Sublime to edit project files
+
+### Install
+
+1. On your computer, open your terminal (For Mac and Linux) or CMD (for Windows)
+2. Navigate to the directory where you would like to run this project from, preferably from your web server directory
+3. Type the following command
+
+> git clone git@github.com:rtonata88/namibia_safaris.git
+
+### Usage
+
+Once the project folder is downloaded to your computer, double click on the _index.html_ file to run the project in the browser.
+
+## Authors
+
+👤 **Richard Chambula**
+
+- GitHub: [@rtonata88](https://github.com/rtonata88)
+- Twitter: [@rtonata](https://twitter.com/rtonata)
+- LinkedIn: [Richard Chambula](https://www.linkedin.com/in/richard-chambula-49198425/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## License and Attributions
+
+This work is licensed under a [Creative Commons Attribution-NonCommercial 4.0 International License](http://creativecommons.org/licenses/by-nc/4.0/)
+
+## Acknowledgements
+
+Original design idea by [Cindy Shin in Behance](https://www.behance.net/adagio07)
---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
+
+const clients = [
+  {
+    id: 1,
+    fullName: 'Peter Andrews',
+    title: 'CEO Lake Wood Hotel',
+    picture: 'assets/images/head-1.jpg',
+    placeholder: 'assets/images/board-01.jpg',
+    message:
+      'We have been working with luxury living for over 10 years and we appreciate their services',
+  },
+  {
+    id: 2,
+    fullName: 'Ray montana',
+    title: 'CEO Lake Wood Hotel',
+    picture: 'assets/images/head-2.jpg',
+    placeholder: 'assets/images/board-01.jpg',
+    message:
+      'We have been working with luxury living for 2 years and we love it here',
+  },
+  {
+    id: 3,
+    fullName: 'Adam lethbridge',
+    title: 'CEO Lake Wood Hotel',
+    picture: 'assets/images/head-3.jpg',
+    placeholder: 'assets/images/board-01.jpg',
+    message: 'Luxury Living is by far the best company',
+  },
+  {
+    id: 4,
+    fullName: 'Mary chama',
+    title: 'CEO Lake Wood Hotel',
+    picture: 'assets/images/head-4.jpg',
+    placeholder: 'assets/images/board-01.jpg',
+    message:
+      'Every time i feel like taking a break i engage luxury living to help me relax',
+  },
+  {
+    id: 5,
+    fullName: 'Sarah gale',
+    title: 'CEO Lake Wood Hotel',
+    picture: 'assets/images/head-5.jpg',
+    placeholder: 'assets/images/board-01.jpg',
+    message: 'Luxury living is simply the best',
+  },
+  {
+    id: 6,
+    fullName: 'Michael Stewart',
+    title: 'CEO Lake Wood Hotel',
+    picture: 'assets/images/head-6.jpg',
+    placeholder: 'assets/images/board-01.jpg',
+    message: 'Amazing company',
+  },
+];
+
+const myContent = document.querySelector('.myContent');
+
+const showInHtml = clients
+  .map(
+    (client) => `
+        <div class="col d-flex">
+            <div class="image-box d-flex me-4">
+              <img class="square-img" src=${client.placeholder} alt="" />
+              <img class="person" src=${client.picture} alt="" />
+            </div>
+            <div class="text-box">
+              <h3>${client.fullName}</h3>
+              <hr class="horizontal-rule" style="border: 2px solid #ec5242; />
+              <span class="fst-italic">${client.title}</span>
+              <p>
+                ${client.message}
+              </p>
+            </div>
+        </div>
+    `,
+  )
+  .join('');
+
+myContent.innerHTML = showInHtml;
